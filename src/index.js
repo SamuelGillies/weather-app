bb8a47ae4ecdd76fc47ad12b5c8c8e5f8e65bf74
Sam Gillies <sam@samgillies.com>
2023-04-12 11:13:54 +0100

    object with weather data returns

--- src/index.js
+++ src/index.js
@@ -1,4 +1,27 @@
 import './style.css'; 
 // import img from './img/'
 
-const k = '6e6c524233e149aa8be130049230604'; 
+const key = '6e6c524233e149aa8be130049230604';
+let location = 'london';
+
+function generateDOM() {
+
+
+}
+
+async function getWeather() {
+    let weather = {}; 
+    const responseData = await fetch(`https://api.weatherapi.com/v1/current.json?key=${key}&q=${location}`, {mode: 'cors'}); 
+    const weatherData = await responseData.json(); 
+    weather.cTemp = weatherData.current.temp_c;
+    weather.cTempFeel = weatherData.current.feelslike_c;
+    weather.humidity = weatherData.current.humidity;
+    weather.wind = weatherData.current.wind_kph;
+    weather.uv = weatherData.current.uv;
+    
+    console.log(weatherData);
+    console.log(weather);
+
+}
+
+getWeather(); 
