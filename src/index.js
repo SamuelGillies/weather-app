35e8dacd8b8f08b762bf0533e9593f73b73b50fa
Sam Gillies <sam@samgillies.com>
2023-04-13 15:02:52 +0100

    weather API data displays on page, DOM generates

--- src/index.js
+++ src/index.js
@@ -4,40 +4,90 @@ import './style.css';
 function generateDOM(newData) {
     const element = document.getElementById('container'); 
 
-    const formContainer = document.createElement('div'); 
+    //Search form DOM generation
+
+    let formContainer = document.createElement('div'); 
     formContainer.setAttribute('id', 'form--container');
-    const form = document.createElement('form'); 
+    let form = document.createElement('form'); 
     form.setAttribute('id', 'form');
-    const search = document.createElement('input');
+    let search = document.createElement('input');
     search.setAttribute('id', 'form--search');
     search.setAttribute('type', 'text');
-    const submit = document.createElement('input');
+    let submit = document.createElement('input');
     submit.setAttribute('id', 'form--submit');
     submit.setAttribute('type', 'submit');
     submit.setAttribute('value', 'submit');
 
     form.append(search, submit); 
     formContainer.append(form); 
-    element.append(formContainer); 
 
-    const weatherContainer = document.createElement('div'); 
-    formContainer.setAttribute('id', 'weather--container');
+    //Weather data DOM generation
+
+    let weatherContainer = document.createElement('div'); 
+    weatherContainer.setAttribute('id', 'weather--container');
     
+    let locationContainer = document.createElement('div'); 
+    locationContainer.setAttribute('id', 'location--container');
+    let nameText = document.createElement('p'); 
+    nameText.setAttribute('id', 'location--nameText');
+    nameText.setAttribute('class', 'text');
+    nameText.textContent = `Location: ${newData.name}, ${newData.region}`; 
+    let descText = document.createElement('p'); 
+    descText.setAttribute('id', 'location--descText');
+    descText.setAttribute('class', 'text');
+    descText.textContent = newData.description; 
 
+    locationContainer.append(nameText, descText); 
 
+    let tempContainer = document.createElement('div'); 
+    tempContainer.setAttribute('id', 'temperature--container');
+    let tempText = document.createElement('p'); 
+    tempText.setAttribute('id', 'temperature--text');
+    tempText.setAttribute('class', 'text');
+    tempText.textContent = newData.temp; 
+
+    tempContainer.append(tempText); 
+
+    let infoContainer = document.createElement('div'); 
+    infoContainer.setAttribute('id', 'info--container');
+    let feelslikeText = document.createElement('p'); 
+    feelslikeText.setAttribute('id', 'info--feelslikeText');
+    feelslikeText.setAttribute('class', 'text');
+    feelslikeText.textContent = `Feels like: ${newData.feel}˚`;  
+    let humidityText = document.createElement('p'); 
+    humidityText.setAttribute('id', 'info--humidityText');
+    humidityText.setAttribute('class', 'text');
+    humidityText.textContent = `Humidity: ${newData.humidity}`;  
+    let windText = document.createElement('p'); 
+    windText.setAttribute('id', 'info--windText');
+    windText.setAttribute('class', 'text');
+    windText.textContent = `Wind Speed: ${newData.humidity}kph`;  
+    let uvText = document.createElement('p'); 
+    uvText.setAttribute('id', 'info--uvText');
+    uvText.setAttribute('class', 'text');
+    uvText.textContent = `UV Rating: ${newData.humidity}`;  
+
+    infoContainer.append(feelslikeText, humidityText, windText, uvText); 
+
+    weatherContainer.append(locationContainer, tempContainer, infoContainer);
+    element.append(formContainer, weatherContainer); 
 
 }
 
 async function getWeather(location) {
     const key = '6e6c524233e149aa8be130049230604';
-    const responseData = await fetch(`https://api.weatherapi.com/v1/current.json?key=${key}&q=${location}`, {mode: 'cors'}); 
-    const weatherData = await responseData.json(); 
-    const newData = processData(weatherData); 
+    let responseData = await fetch(`https://api.weatherapi.com/v1/current.json?key=${key}&q=${location}`, {mode: 'cors'}); 
+    let weatherData = await responseData.json(); 
+    let newData = processData(weatherData);
     generateDOM(newData); 
 }
 
 function processData(weatherData) {
-    const data = {
+    let data = {
+        name: weatherData.location.name, 
+        region: weatherData.location.region, 
+        description: weatherData.current.condition.text,
+        icon: weatherData.current.condition.icon,  
         temp: weatherData.current.temp_c, 
         feel: weatherData.current.feelslike_c,
         humidity: weatherData.current.humidity, 
